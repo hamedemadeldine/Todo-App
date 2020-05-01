3ed431d0865674c02bd2f8b8b143274c2f1bfb42
hamed emad <hamadaemadeldine@gmail.com>
2020-05-01 06:19:00 +0200

    Updates

---
+++ precache-manifest.b675235b3cc86c23a3f408161fce0e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fa8491d5a966c4d0a15824041f47b6",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "72d16727701214b145ec",
+    "url": "/todo-app/static/css/main.f64bd59d.chunk.css"
+  },
+  {
+    "revision": "d3690df18e59e9f510a3",
+    "url": "/todo-app/static/js/2.f2689343.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-app/static/js/2.f2689343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d16727701214b145ec",
+    "url": "/todo-app/static/js/main.b25d8b97.chunk.js"
+  },
+  {
+    "revision": "d8251b2f385fe689bb26",
+    "url": "/todo-app/static/js/runtime-main.e1782121.js"
+  }
+]);
